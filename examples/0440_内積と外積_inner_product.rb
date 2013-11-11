503f30f1a36c87eedb09f494a852be8a5aa7ff6d
akicho8 <akicho8@gmail.com>
2013-11-11 23:47:29 +0900

    outer_product → cross_product

--- examples/0440_内積と外積_inner_product.rb
+++ examples/0440_内積と外積_inner_product.rb
@@ -22,13 +22,13 @@ class App < Stylet::Base
     a, b = @points
     # Bを動かしているときにはAをnormalizeした方がわかりやすい
     inner_product = Stylet::Vector.inner_product(a.normalize, b)
-    outer_product = Stylet::Vector.outer_product(a.normalize, b)
+    cross_product = Stylet::Vector.cross_product(a.normalize, b)
 
     vputs "内積(横): #{inner_product.round(2)}"
-    vputs "外積(縦): #{outer_product.round(2)}"
+    vputs "外積(縦): #{cross_product.round(2)}"
 
     vC = a.normalize * inner_product
-    vD = a.normalize.rotate(Stylet::Fee.r90) * outer_product
+    vD = a.normalize.rotate(Stylet::Fee.r90) * cross_product
     draw_vector(vC, :origin => rect.center, :color => "orange")
     draw_vector(vD, :origin => vC + rect.center, :color => "orange")
   end
