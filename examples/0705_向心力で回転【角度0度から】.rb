71b26cebfd0d0948fee16190b939c6f88272fc49
akicho8 <akicho8@gmail.com>
2013-12-15 21:07:39 +0900

    向心力で回転する例追加

---
+++ examples/0705_向心力で回転【角度0度から】.rb
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# sin/cosを一切使わずに回転する方法(角度0から)
+#
+require_relative "helper"
+
+class App < Stylet::Base
+  setup do
+    @speed = (2 * Math::PI / (60 * 8))       # 角速度 (一周2πを120分割 = 8秒で一周想定)
+    @r = rect.height / 4                     # 半径
+    @p = Stylet::Vector.new(@r, 0)           # 座標 (0度の状態では)
+    @v = Stylet::Vector.new(0, @r * @speed)  # 速度 (右端にいるのでy方向のベクトルだけが生きている)
+  end
+
+  update do
+    a = @p * -@speed ** 2      # 加速度 = 現在の座標 * -角速度^2
+    @v += a                    # 速度 += 加速度
+    @p += @v                   # 座標 += 速度
+    draw_vector(@p)
+    vputs "@v: #{@v}"
+  end
+
+  run
+end
---
+++ examples/0706_向心力で回転【任意の角度から】.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# sin/cosを一切使わずに回転【任意の角度から】
+#
+require_relative "helper"
+
+class App < Stylet::Base
+  setup do
+    @speed = (2 * Math::PI / (60 * 8))       # 角速度 (一周2πを120分割 = 8秒で一周想定)
+
+    @r = 100                                 # 半径
+    @p = Stylet::Vector.angle_at(Stylet::Fee.r45) * @r # 座標(45度=右下)
+
+    # 速度の求め方
+    # vx = -rω * sin(ωt)
+    # vy = rω * cos(ωt)
+    t = Math.atan2(@p.y, @p.x) / @speed     # tフレーム目
+    @v = Stylet::Vector.new
+    @v.x = -@r * @speed * Math.sin(@speed * t)
+    @v.y =  @r * @speed * Math.cos(@speed * t)
+    # ※ 0, 90, 180, 270 度から始まるなら sin cos は不要になる
+  end
+
+  update do
+    a = @p * -@speed ** 2      # 加速度 = 現在の座標 * -角速度^2
+    @v += a                    # 速度 += 加速度
+    @p += @v                   # 座標 += 速度
+    draw_vector(@p)
+    vputs "@v: #{@v}"
+  end
+
+  run
+end
