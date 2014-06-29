24deeb2b942b2a1b671366c3fd5a71205b75f237
akicho8 <akicho8@gmail.com>
2014-06-30 01:38:31 +0900

    close → close_and_parent_restart

--- examples/0950_menu.rb
+++ examples/0950_menu.rb
@@ -7,11 +7,11 @@ menu = Stylet::Menu::Basic.new(name: "[メニュー]", elements: [
         s.chain(name: "[サブメニュー]", elements: [
             {name: "A", safe_command: proc { p 1 }},
             {name: "B", safe_command: proc { p 2 }},
-            {name: "閉じる", safe_command: :close },
+            {name: "閉じる", safe_command: :close_and_parent_restart },
           ])
       }},
     {name: "サブメニュー2", safe_command: proc { s.chain(name: "[サブメニュー2]", elements: 16.times.collect{|i|{:name => "項目#{i}"}})}},
-    {:name => "閉じる", :safe_command => :close },
+    {:name => "閉じる", :safe_command => :close_and_parent_restart },
   ])
 
 Stylet.run {|s| menu.update }
