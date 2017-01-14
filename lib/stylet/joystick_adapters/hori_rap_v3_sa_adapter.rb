ac6e9e6afe2f83b7dfdaa3ece42657e64f06e882
akicho8 <akicho8@gmail.com>
2017-01-14 22:50:49 +0900

    HORI RAP V3 SA のレバーがおかしい→修正

--- lib/stylet/joystick_adapters/hori_rap_v3_sa_adapter.rb
+++ lib/stylet/joystick_adapters/hori_rap_v3_sa_adapter.rb
@@ -5,6 +5,11 @@
 # ＋ 0 3 5 4
 #    1 2 7 6  9(START)
 #
+# ▼スイッチと axis(インデックス) の対応
+#
+#   LS -> 0, 1
+#   PR -> 2, 3
+#
 module Stylet
   module JoystickAdapters
     class HoriRapV3SaAdapter < JoystickAdapter
@@ -22,15 +27,30 @@ module Stylet
       end
 
       def lever_on?(dir)
+        # 以前まではニュートラルが0だったのでこの方法でよかった
+        # case dir
+        # when :up
+        #   axis(1).negative?
+        # when :down
+        #   axis(1).positive?
+        # when :right
+        #   axis(0).positive?
+        # when :left
+        #   axis(0).negative?
+        # else
+        #   false
+        # end
+
+        # macOS Sierra にしたらニュートラルが 128 になってしまったので汎用的な判別方法に変更。
         case dir
         when :up
-          axis(1).negative?
+          axis(1) <= -32768
         when :down
-          axis(1).positive?
+          axis(1) >= 32767
         when :right
-          axis(0).positive?
+          axis(0) >= 32767
         when :left
-          axis(0).negative?
+          axis(0) <= -32768
         else
           false
         end
