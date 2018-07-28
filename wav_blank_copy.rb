b0c787b35b074ffd9cfb19d120f28ccc0af0b92f
akicho8 <akicho8@gmail.com>
2018-07-28 10:59:07 +0900

    [chore] いらないスクリプトを other に移動して other は ignore

--- wav_blank_copy.rb
+++
@@ -1,9 +0,0 @@
-# 58バイトの WAV を blank.wav で置き換える
-require "pathname"
-require "shellwords"
-path = "assets/audios/replacement/**/*.{WAV,wav}"
-Pathname.glob(Pathname(path).expand_path) {|in_file|
-  if in_file.size == 58
-    system "cp -v blank.wav #{in_file.to_s.shellescape}"
-  end
-}
