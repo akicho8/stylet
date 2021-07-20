def6da336e3487c8737a0953c5a2e13a9145ba6b
akicho8 <akicho8@gmail.com>
2021-07-20 20:55:45 +0900

    [refactor] test_video1 削除

--- test_sdl2/test_event_poll.rb
+++ test_sdl2/test_event_poll.rb
@@ -5,7 +5,8 @@ SDL2.init(SDL2::INIT_EVERYTHING)
 loop do
   while ev = SDL2::Event.poll
     p ev
-    if ev.kind_of?(SDL2::Event::KeyDown)
+    case ev
+    when SDL2::Event::KeyDown
       p ev.scancode
       if ev.scancode == SDL2::Key::Scan::ESCAPE
         exit
@@ -16,4 +17,3 @@ loop do
     end
   end
 end
-
