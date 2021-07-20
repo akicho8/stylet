f348536b6b54ce08567ace9abaed52a1a127a272
akicho8 <akicho8@gmail.com>
2021-07-20 20:55:31 +0900

    [feat] add test_video_window

---
+++ test_sdl2/test_video_window.rb
@@ -0,0 +1,21 @@
+require "./setup"
+
+SDL2.init(SDL2::INIT_EVERYTHING)
+
+window = SDL2::Window.create("(WindowTitle)", SDL2::Window::POS_CENTERED, SDL2::Window::POS_CENTERED, 640, 480, 0)
+renderer = window.create_renderer(-1, SDL2::Renderer::Flags::PRESENTVSYNC)
+
+loop do
+  while ev = SDL2::Event.poll
+    case ev
+    when SDL2::Event::KeyDown
+      if ev.scancode == SDL2::Key::Scan::ESCAPE
+        exit
+      end
+      if ev.scancode == SDL2::Key::Scan::Q
+        exit
+      end
+    end
+  end
+  renderer.present
+end
