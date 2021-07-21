898da59e7e5a93e1329b791f7a1f4aa313a01320
akicho8 <akicho8@gmail.com>
2021-07-21 22:18:00 +0900

    [feat] add test_version

--- test_sdl2/test_version.rb
+++ test_sdl2/test_version.rb
@@ -1,2 +1,13 @@
 require "./setup"
-SDL2::VERSION                   # => "0.3.5"
+SDL2::VERSION                     # => "0.3.5"
+SDL2::VERSION_NUMBER              # => [0, 3, 5]
+SDL2::LIBSDL_VERSION              # => "2.0.14"
+SDL2::LIBSDL_VERSION_NUMBER       # => [2, 0, 14]
+SDL2::LIBSDL_REVISION             # => "hg-14525:e52d96ea04fc"
+SDL2::LIBSDL_REVISION_NUMBER      # => 14525
+SDL2::LIBSDL_IMAGE_VERSION        # => "2.0.5"
+SDL2::LIBSDL_IMAGE_VERSION_NUMBER # => [2, 0, 5]
+SDL2::LIBSDL_TTF_VERSION          # => "2.0.15"
+SDL2::LIBSDL_TTF_VERSION_NUMBER   # => [2, 0, 15]
+SDL2::LIBSDL_MIXER_VERSION        # => "2.0.4"
+SDL2::LIBSDL_MIXER_VERSION_NUMBER # => [2, 0, 4]
