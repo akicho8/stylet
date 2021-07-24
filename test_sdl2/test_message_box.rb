82bcc6564442b1c60134efa4dd693d1bceab9ce0
akicho8 <akicho8@gmail.com>
2021-07-24 21:52:54 +0900

    [feat] add test_message.rb

---
+++ test_sdl2/test_message_box.rb
@@ -0,0 +1,33 @@
+require "./setup"
+
+selected_id = SDL2::MessageBox.show({
+    flags: SDL2::MessageBox::WARNING,
+    window: nil,
+    title: "タイトル",
+    message: "メッセージ",
+    buttons: [
+      {
+        # flags is ignored
+        id: 0,
+        text: "No",
+      },
+      {
+        flags: SDL2::MessageBox::BUTTON_RETURNKEY_DEFAULT,
+        id: 100,
+        text: "Yes",
+      },
+      {
+        flags: SDL2::MessageBox::BUTTON_ESCAPEKEY_DEFAULT,
+        id: 2,
+        text: "Cancel",
+      },
+    ],
+    color_scheme: {
+      bg: [255, 0, 0],
+      text: [0, 255, 0],
+      button_border: [255, 0, 0],
+      button_bg: [0, 0, 255],
+      button_selected: [255, 0, 0]
+    },
+  })
+selected_id                     # => 100
